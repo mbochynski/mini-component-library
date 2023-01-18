e6bbcf0a5063497f9f2db2383792d8f47b50cb1b
Mateusz Bochynski <mbochynski@pgs-soft.com>
2023-01-18 18:19:09 +0100

    add discloimer

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,9 @@ import { COLORS } from "../../constants";
 import Icon from "../Icon";
 import VisuallyHidden from "../VisuallyHidden";
 
+/* disclaimer - this component is not the finest (pixel perfect or color perfect)
+   I've focused on most tricky part (in my opinion) only
+*/
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
   return (
     <div>
